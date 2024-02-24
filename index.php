b4c1a815b7c882e8af447e3db45ae5b4c1bab5c2
AndriRy <140690933+AndriRy@users.noreply.github.com>
2024-02-25 00:21:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php include "header.php"; ?>
+<div id="contoh-carousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#contoh-carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#contoh-carousel" data-slide-to="1"></li>
+          <li data-target="#contoh-carousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="gambar/111.jpg" width="1400" height="600" class="img-thumbnail img-responsive" alt="oi">
+            <div class="carousel-caption">
+              <h1><b>INSTITUT TEKNOLOGI BANDUNG</b></h1>
+            </div>
+          </div>
+          <div class="item">
+            <img src="gambar/112.jpg"  width="1400" height="600"class="img-thumbnail img-responsive" alt="ea">
+            <div class="carousel-caption">
+            <h1><b>INSTITUT TEKNOLOGI BANDUNG</b></h1>
+            </div>
+          </div>
+          <div class="item">
+            <img src="gambar/113.jpg" width="1400" height="600" class="img-thumbnail img-responsive" alt="okelah">
+            <div class="carousel-caption">
+            <h1><b>INSTITUT TEKNOLOGI BANDUNG</b></h1>
+            </div>
+          </div>
+      </div>
+      <a class="left carousel-control" href="#contoh-carousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#contoh-carousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+      </a>
+      </div>
+<br>
+	<br>
+	<br>
+	<div class="container">
+		<div class="row">
+			<div style="text-align:center;" >
+			<h2><p style="font-family: times new roman"><b>Institut Teknologi Bandung</b></p></h2>
+			<br>
+		</div>
+				<br>
+	            </div> 
+	            <div class="col-md-16">
+	            	<div style="text-align:justify;">
+	            		<h3><b>Sambutan</b></h3>
+		            	<p>Om Swastiastu,
+		            		<br>
+							Assalamualaikum Warahmatullahi Wabarakatuh,
+							<br>
+							Salam sejahtera buat kita semua,
+							<br>
+							Namo Buddhaya.
+							<br>
+							Para Hadirin yang Terhormat,
+
+                                          Saya berdiri di hadapan Anda semua dengan rasa bangga dan haru, karena hari ini merupakan awal dari perjalanan yang baru bagi kita semua. Saya mengucapkan selamat datang kepada Anda di acara pembukaan Kampus Baru yang akan menjadi bagian integral dari komunitas pendidikan kami.
+
+                                          Saya ingin memulai dengan mengucapkan terima kasih yang tulus kepada semua pihak yang telah mendukung dan membantu kami dalam mewujudkan mimpi ini. Tanpa kerjasama dan dedikasi dari banyak pihak, mimpi untuk membuka Kampus Baru ini tidak akan menjadi kenyataan.
+
+                                          Kampus Baru ini bukan hanya tentang bangunan fisik, tetapi juga tentang aspirasi, harapan, dan impian. Ini adalah tempat di mana generasi mendatang akan menemukan pengetahuan, mengasah bakat, dan mengejar mimpi mereka dengan tekad yang tak tergoyahkan.
+
+                                          Sebagai calon pemilik kampus, saya bertekad untuk memastikan bahwa Kampus Baru ini tidak hanya menjadi tempat untuk memperoleh pendidikan, tetapi juga menjadi pusat inovasi, pembelajaran berkelanjutan, dan pendorong perubahan positif dalam masyarakat.
+
+                                          Kami berkomitmen untuk menciptakan lingkungan yang inklusif, di mana setiap individu dihargai dan didorong untuk mencapai potensi tertingginya. Dengan fasilitas modern, kurikulum yang relevan, dan staf pengajar yang berkualitas, kami percaya bahwa Kampus Baru ini akan menjadi tempat yang luar biasa untuk mengembangkan bakat dan mempersiapkan generasi masa depan.
+
+                                          Saya mengundang Anda semua untuk bergabung dengan kami dalam perjalanan ini. Mari kita bersama-sama menciptakan masa depan yang cerah bagi pendidikan dan masyarakat kita. Terima kasih atas kehadiran dan dukungan Anda.
+
+                                          Salam hormat.<br>
+							<br>
+							<br>
+	
+						</p>
+	            	</div>
+					<div class="jumbotron">
+            <div class="container">
+                  <div class="row">
+                        <div class="col-md-12"> 
+                          <h1><b>Profile Universitas</b></h1>
+                        </div>
+                  </div>
+            </div>
+</div>
+					<div class="container">
+            <div class="row">
+                  <div class="col-md-6">
+                  <p>Universitas Garut berdiri pada tanggal 21 Desember 1998 yang disahkan melalui SK Menteri 
+                  Pendidikan dan Kebudayaan Republik Indonesia dengan nomor 173/D/O/1998. Berdirinya Universitas 
+                  Garut dimulai pada saat Yayasan Perguruan Tinggi Garut mendirikan Sekolah Tinggi Ilmu 
+                  Kemasyarakatan (STIK) dengan 1 program studi yaitu Kesejahteraan Sosial. Berkat perjuangan yang 
+                  cukup keras dari para pengelola perguruan tinggi tersebut, dan dengan meningkatnya pula kebutuhan
+                  akan pendidikan tinggi oleh masyarakat Garut, maka dilakukan penambahan program studi baru, yaitu 
+                  Ilmu Administrasi dan Pekerja Sosial Medik.
+                  Pada tahun 1991 Yayasan Perguruan Tinggi Garut mendirikan Sekolah Tinggi Ilmu Ekonomi dengan program 
+                  studi Manajemen dan Akuntansi. Selanjutnya Pada tahun 1996 didirikan program Pascasarajana dengan 
+                  jurusan Ilmu Administrasi, konsentrasi Administrasi Negara. Kemudian pada tahun 1997, Yayasan 
+                  Perguruan Tinggi Garut mendirikan Fakultas Agama Islam dengan program studi Kependidikan Islam, 
+                  Pendidikan Agama Islam serta Pendidikan Guru Sekolah Dasar.
+                  Garut sebagai Kabupaten yang sebagaian besar masyarakatnya berprofesi sebagai petani, memberikan 
+                  peluang untuk pendirian sebuah lembaga pendidikan tinggi pertanian. Pada tahun 1992, Yayasan 
+                  Gilang Kencana yang juga mengelola pendidikan dasar menengah, mendirikan Sekolah Tinggi Pertanian 
+                  Gilang Kencana Garut, yang mempunyai 2 program studi yaitu Budidaya Pertanian dan Studi Peternakan.
+                  Sesuai dengan perkembangan zaman dan tuntutan akan pendidikan tinggi yang dirasakan oleh masyarakat 
+                  Garut. Pada tahun 1996 berdiri pula dua buah perguruan tinggi dibawah pengelolaan Yayasan Prima Garut 
+                  yaitu Sekolah Tinggi Farmasi dan Akademi Tekstil. Guna mewujudkan cita-cita masyarakat Garut akan 
+                  berdirinya Universitas Garut, tiga Yayasan penyelenggara perguruan tinggi swasta tersebut di atas; 
+                  Yayasan Perguruan Tinggi Garut, Yayasan Perguruan Prima dan Yayasan Gilang Kencana telah sepakat 
+                  untuk bergabung dalam satu wadah yakni: Yayasan Universitas Garut (YUNGA) yang 
+                  berdiri dengan akta notaris Yayah Kusnariah, SH Nomor 7 tanggal 15 Juli 1997. Yayasan ini berfungsi 
+                  sebagai penyelenggara Universitas Garut.</p>
+            </div>
+            <div class="row">
+                  <div class="col-md-4">
+                        <img src="gambar/kampus.png" width="400" height="300" alt="gambar 2">
+                  </div>
+            </div>
+
+      </div><br><br>
+	            </div> 
+
+			        </div>
+			      </div>
+			    </div>
+	        </div>
+	    </div>
+	</div>
+</div>
+
+<?php include "footer.php"; ?>
---
+++ profile.php
@@ -0,0 +1,51 @@
+<?php include "header.php"; ?>
+
+        <div class="jumbotron">
+            <div class="container">
+                  <div class="row">
+                        <div class="col-md-12"> 
+                          <h1>Profile Universitas</h1>
+                        </div>
+                  </div>
+            </div>
+      </div>   
+
+
+      <div class="container">
+            <div class="row">
+                  <div class="col-md-6">
+                  <p>Universitas Garut berdiri pada tanggal 21 Desember 1998 yang disahkan melalui SK Menteri 
+                  Pendidikan dan Kebudayaan Republik Indonesia dengan nomor 173/D/O/1998. Berdirinya Universitas 
+                  Garut dimulai pada saat Yayasan Perguruan Tinggi Garut mendirikan Sekolah Tinggi Ilmu 
+                  Kemasyarakatan (STIK) dengan 1 program studi yaitu Kesejahteraan Sosial. Berkat perjuangan yang 
+                  cukup keras dari para pengelola perguruan tinggi tersebut, dan dengan meningkatnya pula kebutuhan
+                  akan pendidikan tinggi oleh masyarakat Garut, maka dilakukan penambahan program studi baru, yaitu 
+                  Ilmu Administrasi dan Pekerja Sosial Medik.
+                  Pada tahun 1991 Yayasan Perguruan Tinggi Garut mendirikan Sekolah Tinggi Ilmu Ekonomi dengan program 
+                  studi Manajemen dan Akuntansi. Selanjutnya Pada tahun 1996 didirikan program Pascasarajana dengan 
+                  jurusan Ilmu Administrasi, konsentrasi Administrasi Negara. Kemudian pada tahun 1997, Yayasan 
+                  Perguruan Tinggi Garut mendirikan Fakultas Agama Islam dengan program studi Kependidikan Islam, 
+                  Pendidikan Agama Islam serta Pendidikan Guru Sekolah Dasar.
+                  Garut sebagai Kabupaten yang sebagaian besar masyarakatnya berprofesi sebagai petani, memberikan 
+                  peluang untuk pendirian sebuah lembaga pendidikan tinggi pertanian. Pada tahun 1992, Yayasan 
+                  Gilang Kencana yang juga mengelola pendidikan dasar menengah, mendirikan Sekolah Tinggi Pertanian 
+                  Gilang Kencana Garut, yang mempunyai 2 program studi yaitu Budidaya Pertanian dan Studi Peternakan.
+                  Sesuai dengan perkembangan zaman dan tuntutan akan pendidikan tinggi yang dirasakan oleh masyarakat 
+                  Garut. Pada tahun 1996 berdiri pula dua buah perguruan tinggi dibawah pengelolaan Yayasan Prima Garut 
+                  yaitu Sekolah Tinggi Farmasi dan Akademi Tekstil. Guna mewujudkan cita-cita masyarakat Garut akan 
+                  berdirinya Universitas Garut, tiga Yayasan penyelenggara perguruan tinggi swasta tersebut di atas; 
+                  Yayasan Perguruan Tinggi Garut, Yayasan Perguruan Prima dan Yayasan Gilang Kencana telah sepakat 
+                  untuk bergabung dalam satu wadah yakni: Yayasan Universitas Garut (YUNGA) yang 
+                  berdiri dengan akta notaris Yayah Kusnariah, SH Nomor 7 tanggal 15 Juli 1997. Yayasan ini berfungsi 
+                  sebagai penyelenggara Universitas Garut.</p>
+            </div>
+            <div class="row">
+                  <div class="col-md-6">
+                        <img src="gambar/kampus.png" width="500" height="400" alt="gambar 2">
+                  </div>
+            </div>
+
+      </div><br><br>
+
+
+    <?php include "footer.php"; ?>
